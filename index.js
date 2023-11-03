e5ff8998a48f2520c81912da792fcccb87724dfa
CodeSandbox <support@codesandbox.io>
2023-11-03 22:41:41 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello CodeSandbox");
+console.log("Hello dev");
+import { createServer } from "http";
+
+const httpServer = createServer((req, res) => {
+  console.log("Request received");
+  res.end("Response from server");
+});
+
+httpServer.listen(3000);
